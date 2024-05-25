400e0427500d960762856f5b08ef2898fa8d52f7
Кулешов Сергей Львович <sergey.kuleshov@ru.rhenus.com>
2024-05-25 17:58:37 +0300

    Console application is implemented according to the specifications. Application code was tested. Everything is fine.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,54 +1,20 @@
-interface Listable {
-    fun printName()
-}
-
-data class Note(var note: String) : Listable {
-    override fun printName() {
-        println(note)
-    }
-    companion object {
-        const val TEXT_LIST = "Список заметок"
-        const val TEXT_CREATE = "Создать заметку"
-    }
-}
-
-data class Archive(var name: String) : Listable {
-    val notesList : MutableList<Note> = mutableListOf()
-    override fun printName() {
-        println(name)
-    }
-    companion object {
-        const val TEXT_LIST = "Список архивов"
-        const val TEXT_CREATE = "Создать архив"
-    }
-}
-
-class Menu<E : Listable>(val itemsList: MutableList<E?>) {
-    fun printItems(textCreate: String, textList: String, newInstance: (name: String) -> E?) {
-        for (item in itemsList) {
-            println(textList)
-            item?.printName()
-            println(textCreate)
-        }
-        itemsList.add(newInstance("Четвертая заметка"))
-    }
-}
-
 fun main(args: Array<String>) {
-    val zametka = Note("Тест заметки")
-    val zam_text = Note.TEXT_LIST
-    println("Hello World!")
-    zametka.printName()
-    println(zam_text)
-    println("---------------------")
-
-    // Теперь пробуем создать массив заметок и передать его в объект Meny для вывода на экран
-    val listOfNotes = mutableListOf<Note?>()
-    listOfNotes.add(Note("Первая заметка"))
-    listOfNotes.add(Note("Вторая заметка"))
-    listOfNotes.add(Note("Третья заметка"))
-    val menu1 = Menu(listOfNotes)
-    menu1.printItems(Note.TEXT_CREATE, Note.TEXT_LIST) { name -> Note(name) }
-    menu1.printItems(Note.TEXT_CREATE, Note.TEXT_LIST) { name -> Note(name) }
-    menu1.printItems(Note.TEXT_CREATE, Note.TEXT_LIST) { name -> Note(name) }
+    val listOfArchives = mutableListOf<Archive>()
+    val archiveMenu = Menu(listOfArchives)
+    var archive: Archive?
+    var notesMenu: Menu<Note>
+    var note: Note?
+    val inp = UserInput()
+    while (true) {    // Основной цикл работы с архивами
+        archive = archiveMenu.startMenu("Список архивов", "Создать архив", "Введите имя архива") { name -> Archive(name) }
+        if (archive == null) return
+        do {    // Цикл работы с заметками
+            notesMenu = Menu(archive.notesList)
+            note = notesMenu.startMenu("Список заметок", "Создать заметку", "Введите текст заметки") { name -> Note(name) }
+            if (note != null) {
+                println(note.getNameString())
+                inp.getEnter()
+            }
+        } while (note != null)
+    }
 }
